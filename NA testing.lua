5e1b96d5f49282b0d9a73c5822a6f32befdaf5ab
maxype <117316014+ltseverydayyou@users.noreply.github.com>
2024-07-21 00:00:17 +0000

    Update NA testing.lua

--- NA testing.lua
+++ NA testing.lua
@@ -292,14 +292,15 @@ function didYouMean(arg)
 end
 
 local function isRelAdmin(Player)
-	for _,id in ipairs(_G.NAadminsLol) do
-		if Player.UserId==id and game:GetService("Players").LocalPlayer.UserId~=id then
-			return true
-		end
-	end
-	return false
+    for _, id in ipairs(_G.NAadminsLol) do
+        if id == game.Players.LocalPlayer.UserId then
+            return false
+        elseif Player.UserId == id then
+            return true
+        end
+    end
+    return false
 end
-
 --[[ COMMAND FUNCTIONS ]]--
 local commandcount=0
 cmd={}
