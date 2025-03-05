d37c1193024ed910b85d54d825414b646220d230
Null <117316014+ltseverydayyou@users.noreply.github.com>
2025-03-05 23:54:16 +0200

    adonis

--- Source.lua
+++ Source.lua
@@ -140,9 +140,10 @@ local updLogs={
 	log1='Headsit & Headstand commands are fixed and will no longer fling you';
 	log2='updated "bhop" command and gave it a variable name "loopjump"';
 	log3='improved "firetouchinterests" command to function properly';
+	log4='fixed "adonisbypass" command by the_king.78';
 }
 
-local updDate="3/05/2025" --month,day,year
+local updDate="3/06/2025" --month,day,year
 
 --[[ VARIABLES ]]--
 
@@ -1947,9 +1948,68 @@ cmd.add({"teleporttoplace","toplace","ttp"},{"teleporttoplace (PlaceId) (toplace
 	game:GetService("TeleportService"):Teleport(pId)
 end)
 
---fixed-viper
+--made by the_king.78
 cmd.add({"adonisbypass","bypassadonis","badonis","adonisb"},{"adonisbypass (bypassadonis,badonis,adonisb)","bypasses adonis admin detection"},function()
-	loadstring(game:HttpGet("https://github.com/ltseverydayyou/Nameless-Admin/blob/main/adonisbypass.lua?raw=true"))()
+local DebugFunc = getinfo or debug.getinfo
+local IsDebug = false
+local hooks = {}
+
+local DetectedMeth, KillMeth
+
+setthreadidentity(2)
+
+for index, value in getgc(true) do
+    if typeof(value) == "table" then
+        local detected = rawget(value, "Detected")
+        local kill = rawget(value, "Kill")
+    
+        if typeof(detected) == "function" and not DetectedMeth then
+            DetectedMeth = detected
+            
+            local hook
+            hook = hookfunction(DetectedMeth, function(methodName, methodFunc, methodInfo)
+                if methodName ~= "_" then
+                    if IsDebug then
+                        warn("Adonis Detected\nMethod: " .. tostring(methodName) .. "\nInfo: " .. tostring(methodFunc))
+                    end
+                end
+                
+                return true
+            end)
+
+            table.insert(hooks, DetectedMeth)
+        end
+
+        if rawget(value, "Variables") and rawget(value, "Process") and typeof(kill) == "function" and not KillMeth then
+            KillMeth = kill
+            local hook
+            hook = hookfunction(KillMeth, function(killFunc)
+                if IsDebug then
+                    warn("Adonis tried to detect: " .. tostring(killFunc))
+                end
+            end)
+
+            table.insert(hooks, KillMeth)
+        end
+    end
+end
+
+local hook
+hook = hookfunction(getrenv().debug.info, newcclosure(function(...)
+    local functionName, functionDetails = ...
+
+    if DetectedMeth and functionName == DetectedMeth then
+        if IsDebug or not IsDebug then
+            --print("Adonis was bypassed by the_king.78")
+        end
+
+        return coroutine.yield(coroutine.running())
+    end
+    
+    return hook(...)
+end))
+
+setthreadidentity(7)
 end)
 
 --[ LOCALPLAYER ]--
